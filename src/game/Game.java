2574f0333baecfe01fde00c2f291d9766367ee45
brotherbrick <brotherbrick@users.noreply.github.com>
2018-12-07 21:45:37 -0700

    overhauled main operating code for game in main

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,8 +1,17 @@
 package game;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
 
+	
+	Scanner s = new Scanner(System.in);
+	Random rand = new Random();
+	
+	
+	
+	
+	
 	
 }
